01598b1a6227a318ac76cc0829ab08643d78bc8e
Rashed Pervez <rpervez02@gmail.com>
2023-03-14 16:54:10 +0600

    Initial Commit

---
+++ GetContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetContacts
+//
+//  Created by Apple iMac on 14/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
